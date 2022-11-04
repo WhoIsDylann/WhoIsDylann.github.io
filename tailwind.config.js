72f319f8357166db9766fdda6c481b4560c719a3
Dylan <dbc0728@gmail.com>
2022-11-04 12:08:32 -0400

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html","./public/index.js"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'mountbattenPink': '#987284',
+      'etonBlue': '#9DBF9E',
+      'paleSpring': '#D0D6B5',
+      'melon': '#F9B5AC',
+      'grey': '#595959',
+      'greyy': '#7F7F7F',
+      'greyyy': '#A5A5A5',
+      'greyyyy': '#CCCCCC'
+    },
+},
+  plugins: [],
+}
+
+
